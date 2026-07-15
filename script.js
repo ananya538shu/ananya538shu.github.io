12a69db5cd9207e479db240dde820deaa9d623e7
ananya538shu <ananya.v.shukla12@gmail.com>
2026-07-15 14:08:04 +0530

    Implement typing effect and smooth scrolling

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const text = [
+  "Aspiring Data Scientist",
+  "Aspiring Data Analyst",
+  "Power BI Enthusiast"
+];
 
+let count = 0;
+let index = 0;
+let current = "";
+let letter = "";
+
+(function type(){
+
+if(count===text.length){
+count=0;
+}
+
+current=text[count];
+letter=current.slice(0,++index);
+
+document.getElementById("typing").textContent=letter;
+
+if(letter.length===current.length){
+count++;
+index=0;
+setTimeout(type,1200);
+}
+else{
+setTimeout(type,100);
+}
+
+})();
+
+const links=document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link=>{
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
